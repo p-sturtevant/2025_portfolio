3863eed776f45507628fd813afda501a874bf93c
Parker Sturtevant <wsturtevant196@gmail.com>
2025-05-11 11:17:16 -0400

    iniit commit

---
+++ app/components/NavBarItem.jsx
@@ -0,0 +1,12 @@
+export const NavBarItem = ({ title, link }) => {
+  return (
+    <div className="border-off-2 flex w-full items-center justify-center rounded-md bg-amber-100 p-5">
+      <a
+        href={link}
+        className="font-dragon-rough hover: w-fit text-3xl text-purple-700 hover:scale-105 hover:cursor-pointer hover:text-purple-400 hover:transition-all hover:duration-300"
+      >
+        {title}
+      </a>
+    </div>
+  );
+};
